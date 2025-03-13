49beae99b4eaf9de0a4bda023671114903e2ed5e
Luo li <luoli1027097053@gmail.com>
2025-03-13 21:42:02 +0800

    add map

---
+++ src/main/java/org/example/map/HashMapMain.java
@@ -0,0 +1,36 @@
+package org.example.map;
+
+import java.util.HashMap;
+
+public class HashMapMain {
+    public static void main(String[] args) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        hashMap.put("One", 1);
+        hashMap.put("Two", 2);
+        hashMap.put("Three", 3);
+
+        // 获取大小
+        int size = hashMap.size();
+        System.out.println("Size of HashMap: " + size);
+        // 检查是否包含特定键
+        boolean containsKey = hashMap.containsKey("Two");
+        System.out.println("HashMap contains key 'Two': " + containsKey);
+
+        // 检查特定键对应的值
+        int value = hashMap.get("One");
+        System.out.println("'One' maps to value: " + value);
+
+        // 删除特定键值对
+        int removedValue = hashMap.remove("Two");
+        System.out.println("Removed value associated with 'Two': " + removedValue);
+
+        // 打印所有键值对
+        System.out.println("HashMap entries: " + hashMap);
+
+        // clear HashMap
+        hashMap.clear();
+        System.out.println("HashMap after clearing: " + hashMap);
+
+    }
+}
---
+++ src/main/java/org/example/map/WordCounter.java
@@ -0,0 +1,27 @@
+package org.example.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordCounter {
+    public static void main(String[] args) {
+        String document = "this is a sample document. this document contains many words. Some words repeat, some are unique.";
+
+        HashMap<String, Integer> wordCountMap = new HashMap<>();
+
+        String[] words = document.split("[\\s\\p{Punct}]+");
+
+        for (String word : words) {
+            if (wordCountMap.containsKey(word)) {
+                wordCountMap.put(word, wordCountMap.get(word) + 1);
+            } else {
+                wordCountMap.put(word, 1);
+            }
+        }
+
+        // 打印结果
+        for (Map.Entry<String ,Integer> entry : wordCountMap.entrySet()) {
+            System.out.println("单词 '" + entry.getKey() + "'出现了" + entry.getValue() + "次");
+        }
+    }
+}
